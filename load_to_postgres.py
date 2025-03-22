a62a049a44f5001fa048fcc074f4224bc1f04428
0bleak <mr.tamimi.mohammed@gmail.com>
2025-03-22 01:36:49 +0100

    updating model

--- load_to_postgres.py
+++ load_to_postgres.py
@@ -6,7 +6,7 @@ DB_PARAMS = {
     "dbname": os.getenv("DB_NAME", "global_water"),
     "user": os.getenv("DB_USER", "postgres"),
     "password": os.getenv("DB_PASSWORD", "root"),
-    "host": os.getenv("DB_HOST", "localhost"),  # "db" si Docker
+    "host": os.getenv("DB_HOST", "localhost"),  
     "port": os.getenv("DB_PORT", "5432")
 }
 
